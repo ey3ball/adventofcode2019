8972877ed1d6aab1193c90e822bb52fa4a4bfbef
Florian Vallée <fvallee@idmog.com>
2019-12-06 11:06:05 +0100

    day6_2 quick & dirty

--- day6/day6_2.py
+++ day6/day6_2.py
@@ -6,18 +6,55 @@ with open("day6.txt", "r") as f:
     datafile = f.readlines()
 
 orbits = defaultdict(list)
+parents = dict()
 
 for orbit in datafile:
     [around, obj] = orbit.strip().split(")")
     orbits[around].append(obj)
+    parents[obj] = around
+
+def list_parents(node):
+    plist = []
+    while True:
+        if node == "COM":
+            break
+        plist.append(parents[node])
+        node = parents[node]
+    return plist
+
+print("list_parents")
+san = list_parents("SAN")
+you = list_parents("YOU")
+
+common_parents = set(san) & set(you)
+
+first_common = None
+for parent in san:
+    if parent in common_parents:
+        first_common = parent
+        break
+
+print(first_common)
 
 nodes = ["COM"]
 depth = 1
 total_depth = 0
 
+depth_common = 0
+depth_san = 0
+depth_you = 0
+
 while True:
     nodes = sum([orbits[obj] for obj in nodes], [])
     total_depth += len(nodes) * depth
+
+
+    if first_common in nodes:
+        depth_common = depth
+    if "YOU" in nodes:
+        depth_you = depth
+    if "SAN" in nodes:
+        depth_san = depth
 
     depth += 1
 
@@ -25,3 +62,6 @@ while True:
         break
 
 print(total_depth)
+print(depth_common)
+print(depth_you)
+print(depth_san)
